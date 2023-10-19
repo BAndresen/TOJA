45485955b8a4ead63d5c619c0d1d63e6241d1202
Brendan Andresen <brendan.development@pm.me>
2023-10-18 21:33:18 -0700

    UI updates keywords

--- toja/views/home.py
+++ toja/views/home.py
@@ -113,9 +113,6 @@ class HomeView(customtkinter.CTk):
         # Graph - Days vs Events
         self.calendar_frame = customtkinter.CTkFrame(self.home_frame)
         self.calendar_frame.grid(row=2, columnspan=3, column=0, padx=20, pady=20, sticky="nsew")
-        # self.calendar_label_placeholder = (customtkinter.CTkLabel(
-        #     self.calendar_frame, text="graph - days vs events (coming soon)"))
-        # self.calendar_label_placeholder.grid(row=0, column=1, padx=50, pady=50)
 
         # --------------- Event -------------- #
 
@@ -211,7 +208,7 @@ class HomeView(customtkinter.CTk):
 
         start_val = customtkinter.Variable(self,80)
         self.threshold_entry = customtkinter.CTkEntry(self.search_jd_frame, width=40, )
-        self.threshold_label = customtkinter.CTkLabel(self.search_by_label, )
+        self.threshold_label = customtkinter.CTkLabel(self.search_jd_frame, text="Threshold:")
         self.threshold_entry.configure(textvariable=start_val)
 
         # list box job
@@ -297,6 +294,7 @@ class HomeView(customtkinter.CTk):
     def display_job_id(self, event):
         self.position_entry.grid_remove()
         self.threshold_entry.grid_remove()
+        self.threshold_label.grid_remove()
         self.slider_1.grid_remove()
         self.job_id_entry.grid(row=2, column=1, padx=0, sticky='w')
         self.job_id_entry.configure(placeholder_text='Enter Job ID')
@@ -307,6 +305,7 @@ class HomeView(customtkinter.CTk):
         self.position_entry.grid(row=3, column=1, padx=0, sticky='w')
         self.position_entry.configure(placeholder_text='Enter Position')
         self.threshold_entry.grid(row=4, column=2, padx=0, pady=5, sticky='w')
+        self.threshold_label.grid(row=4, column=0, sticky="e", padx=3)
         self.slider_1.grid(row=4, column=1, sticky="w")
         self.radio_button_3.grid(pady=5)
 
@@ -314,11 +313,10 @@ class HomeView(customtkinter.CTk):
         self.job_id_entry.grid_remove()
         self.position_entry.grid_remove()
         self.threshold_entry.grid_remove()
+        self.threshold_label.grid_remove()
         self.slider_1.grid_remove()
         self.radio_button_3.grid(pady=(5, 43))
 
     def show_value(self, value):
-        # self.tooltip.configure(message=int(value))
-        # self.threshold_entry.configure(placeholder_text=int(value))
         v = customtkinter.Variable(self, int(value))
         self.threshold_entry.configure(textvariable=v)
