aaab964f02fc0834d51cb60081073762624d26e8
Brendan Andresen <brendan.development@pm.me>
2023-10-19 10:53:50 -0700

    removed pathing storage in config

--- toja/config.py
+++ toja/config.py
@@ -1,16 +1,19 @@
 import configparser
 from pathlib import Path
 import platform
+import os
 
+import toja.constants as constant
 
 class Config:
     def __init__(self):
         self.system_platform = None
         self.base_dir = Path(__file__).resolve().parent
+        self.job_description_parent = os.path.join(self.base_dir,constant.JOB_DESCRIPTION_DIRECTORY)
         self.config = configparser.ConfigParser()
-        self.config_file = f'{self.base_dir}\\config.ini'
+        self.config_file = f'{self.base_dir}\\{constant.CONFIG_FILE}'
         self.config.read(self.config_file)
-        self.database_path = self.config['database']['database_path']
+        self.database_path = os.path.join(self.base_dir,constant.DATABASE_DIRECTORY,constant.DATABASE_NAME)
         self.user_name = self.config['user']['name']
 
     def is_user_new(self) -> bool:
@@ -18,16 +21,16 @@ class Config:
             return True
 
     def initialize_user(self) -> None:
-        self.database_path = f'{self.base_dir}\\database\\toja_database.db'
-        self.config['database']['job_description_dir'] = job_description
-        self.config['user']['base_dir'] = str(self.base_dir)
-        self.config['database']['database_path'] = self.database_path
+        # self.database_path = f'{self.base_dir}\\database\\toja_database.db'
+        # self.config['database']['job_description_dir'] = job_description
+        # self.config['user']['base_dir'] = str(self.base_dir)
+        # self.config['database']['database_path'] = self.database_path
         new_user = False
         self.config.set('user', 'new_user', str(new_user))
         with open(self.config_file, "w") as file:
             self.config.write(file)
 
-    def set_database_name(self, user_name: str):
+    def set_user_name(self, user_name: str):
         self.config['user']['name'] = user_name
         with open(self.config_file, "w") as file:
             self.config.write(file)
